27c5867239215100a27392162219afbcbf7fb004
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-06-30 13:54:19 +0200

    R

--- shop/models.py
+++ shop/models.py
@@ -34,6 +34,7 @@ class Product(InternalBaseModel):
     regular_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     ingredients = models.ManyToManyField(Ingredient, related_name='products')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
